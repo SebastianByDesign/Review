a8314e68e231d11d681c4140355e82adbfc880ba
Walter Sebastian Carrera <cwalter1994@gmail.com>
2019-09-12 13:49:58 -0400

    playing with javascript

---
+++ js/modules/DataObject.js
@@ -0,0 +1,13 @@
+// make sure to follow this syntax!!
+// export default {
+//     property: "string",
+//     property: number
+// }
+
+export default {
+    name: "Walter",
+    role: "Student",
+    height: "5ft 6in",
+    weight: 135,
+    hairColor: "Brown"
+}
